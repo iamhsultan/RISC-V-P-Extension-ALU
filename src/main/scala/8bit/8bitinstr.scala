c6c94099c1c9bab307b6beccf9868b41318fb9f6
haris <haris.sultanpuri@aalto.fi>
2024-10-17 16:47:57 +0300

    added changes for Review 1

--- src/main/scala/8bit/8bitinstr.scala
+++ src/main/scala/8bit/8bitinstr.scala
@@ -4,21 +4,23 @@ import chisel3.util._
                             ///--N-bit Adder generator--/// 
 class Adder(width : Int) extends Module {
     val io = IO(new Bundle {
-        val a    = Input(UInt(width.W))
-        val b    = Input(UInt(width.W))
+        val a    = Input(UInt((width+1).W))
+        val b    = Input(UInt((width+1).W))
         val cin  = Input(UInt(1.W))
 
-        val s    = Output(UInt(width.W))
+        val s    = Output(UInt((width+1).W))
         val cout = Output(UInt(1.W))
     })
     //function of class Adder//
-    val cout_s = Wire(UInt((width+1).W))  // Width + 1 takes care of the carry bit as well
-    cout_s  := Cat(0.U(1.W),io.a) + Cat(0.U(1.W),io.b) + Cat(0.U(1.W), io.cin)      // concatenating extra bit to capture the carry out from MSB bit
-    io.s    := cout_s((width-1),0)      // Seperating Sum 
-    io.cout := cout_s(width)            // Seperating Carry
+    //val cout_s = Wire(UInt((width+1).W))  // Width + 1 takes care of the carry bit as well
+    //cout_s  := Cat(0.U(1.W),io.a) + Cat(0.U(1.W),io.b) + Cat(0.U(1.W), io.cin)      // concatenating extra bit to capture the carry out from MSB bit
+    io.s    := io.a + io.b + io.cin
+    //
+    //io.s    := cout_s((width-1),0)      // Seperating Sum 
+    io.cout := io.s(width)            // Seperating Carry
 }
 
-                            ///--PADD.B -- SIMD 8bit Addition--///
+///============================PAADD.B -- SIMD 8bit Addition=========================///
 class PAdd8 extends Module {
     val io = IO(new Bundle {
         val rs1 = Input(UInt(32.W))
@@ -40,7 +42,7 @@ class PAdd8 extends Module {
         A8(x).io.b   := io.rs2((x*8+7) , (x*8+0))
         A8(x).io.cin := carryin(x)      // assigned carryin for the adder
 
-        swire(x)     := A8(x).io.s      // 8bit sum from adders assigned to internal wires
+        swire(x)     := A8(x).io.s(7,0)      // 8bit sum from adders assigned to internal wires
         carryin(x+1) := A8(x).io.cout       // carry out from current adder assigned to carry in of next adder      
         
     }
@@ -48,6 +50,33 @@ class PAdd8 extends Module {
     io.rd := Cat(swire(3),swire(2),swire(1),swire(0))
     //cout := carryin(4)    //the carry out from the component to be assined depending on the instruction 
 }
+
+
+///============================PAADD.B -- SIMD 8-bit Signed Averaging Addition=========================///
+class PAADD8 extends Module {
+    val io = IO(new Bundle{
+        val Rs1 = Input(UInt(32.W))
+        val Rs2 = Input(UInt(32.W))
+        val Rd  = Output(UInt(32.W))
+    })
+
+    //function of PAADD8
+    val A8       = Seq.fill(4)(Module(new Adder(8)))
+    val sumshift = Wire(Vec(4,UInt(8.W)))      //have to check about UInt or SInt?????
+
+    
+    for (x <- 0 until 4) {
+        A8(x).io.cin := 0.U
+        A8(x).io.a := Cat(io.Rs1((x*8+7)) , io.Rs1((x*8+7) , (x*8+0)))
+        A8(x).io.b := Cat(io.Rs2((x*8+7)) , io.Rs2((x*8+7) , (x*8+0)))
+
+        sumshift(x)  :=  A8(x).io.s(8,1)
+    }
+    io.Rd := Cat(sumshift(3),sumshift(2),sumshift(1),sumshift(0))
+}
+
+///============================PAADDU.B -- SIMD 8-bit Unsigned Averaging Addition=========================///
+
 
 
 object GenerateVerilog extends App {
--- src/test/scala/test8bit/test8bitinstr.scala
+++ src/test/scala/test8bit/test8bitinstr.scala
@@ -2,6 +2,8 @@ import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
+///============================TESTER====PAADD.B -- SIMD 8bit Addition=========================///
+
 class PAdd8Tester extends AnyFlatSpec with ChiselScalatestTester {
   "PAdd8" should "correctly add 32-bit inputs with 8-bit chunks" in {
     test(new PAdd8) { dut =>
@@ -33,3 +35,57 @@ class PAdd8Tester extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 }
+
+///============================TESTER====PAADD.B -- SIMD 8-bit Signed Averaging Addition=========================///
+
+class PAADD8Tester extends AnyFlatSpec with ChiselScalatestTester {
+  "PAADD8" should "correctly compute signed averaging addition" in {
+    test(new PAADD8) { dut =>
+      // Test Case 1: Rs1 = 0x7F_00_80_00, Rs2 = 0x7F_00_80_00
+      dut.io.Rs1.poke("h7F008000".U) // Rs1 = 127, 0, -128, 0
+      dut.io.Rs2.poke("h7F008000".U) // Rs2 = 127, 0, -128, 0
+      dut.clock.step()
+      // Expected Rd = (127+127)>>1, (0+0)>>1, (-128-128)>>1, (0+0)>>1 = 127, 0, -128, 0 => Rd = 0x7F008000
+      dut.io.Rd.expect("h7F008000".U)
+
+      // Test Case 2: Rs1 = 0xFF_01_02_80, Rs2 = 0x00_FF_7E_7F
+      dut.io.Rs1.poke("hFF010280".U) // Rs1 = -1, 1, 2, -128
+      dut.io.Rs2.poke("h00FF7E7F".U) // Rs2 = 0, -1, 126, 127
+      dut.clock.step()
+      // Expected Rd = (-1+0)>>1, (1-1)>>1, (2+126)>>1, (-128+127)>>1 => -1, 0, 64, -1 => Rd = 0xFF0040FF
+      dut.io.Rd.expect("hFF0040FF".U)
+
+      // Test Case: Normal Addition and Averaging
+      dut.io.Rs1.poke("h12_34_56_78".U) 
+      dut.io.Rs2.poke("h10_20_30_40".U)  
+      dut.clock.step()
+      // Expected Rd = 0x22 >> 1 = 0x11, 0x54 >> 1 = 0x2A, 0x86 >> 1 = 0x43, 0xB8 >> 1 = 0x5C
+      dut.io.Rd.expect("h11_2A_43_5C".U)
+
+      // Test Case: Addition with Overflow
+      dut.io.Rs1.poke("h7F_7F_7F_7F".U) 
+      dut.io.Rs2.poke("h7F_7F_7F_7F".U) 
+      dut.clock.step()
+      // Expected 8bit elements in Rd  = 0xFE >> 1 = 0x7F
+      dut.io.Rd.expect("h7F_7F_7F_7F".U)
+
+      // Test Case: Addition with Negative Values
+      dut.io.Rs1.poke("h80_80_80_80".U)  
+      dut.io.Rs2.poke("h80_80_80_80".U) 
+      dut.clock.step()
+      // Expected Rd  = 
+      dut.io.Rd.expect("h80_80_80_80".U)
+
+      // Test Case: Mixed Positive and Negative Values
+      dut.io.Rs1.poke("hFF_7F_80_01".U) 
+      dut.io.Rs2.poke("h01_80_7F_FF".U) 
+      dut.clock.step()
+      // Expected Rd  = 
+      dut.io.Rd.expect("h00_FF_FF_00".U)
+    }
+  }
+}
+
+
+///============================TESTER====PAADDU.B -- SIMD 8-bit Unsigned Averaging Addition=========================///
+
